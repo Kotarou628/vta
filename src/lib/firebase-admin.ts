981c3437fdd442cd2e23dd113dd9e34b55d3ddac
Kotarou628 <Tate62Tora8@gmail.com>
2026-04-13 22:29:52 +0900

    修正

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,15 +1,24 @@
-// lib/firebase-admin.ts
-import { cert, getApps, initializeApp } from 'firebase-admin/app';
+import { cert, getApps, initializeApp, App } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceAccount = JSON.parse(
-  Buffer.from(process.env.FIREBASE_ADMIN_KEY_BASE64 as string, 'base64').toString('utf8')
-);
+let app: App;
+
+// 環境変数があるかチェック
+const base64Key = process.env.FIREBASE_ADMIN_KEY_BASE64;
 
 if (!getApps().length) {
-  initializeApp({
-    credential: cert(serviceAccount),
-  });
+  if (base64Key) {
+    // 環境変数がある場合のみ初期化を実行
+    const serviceAccount = JSON.parse(
+      Buffer.from(base64Key, 'base64').toString('utf8')
+    );
+    app = initializeApp({
+      credential: cert(serviceAccount),
+    });
+  } else {
+    // ビルド時など、キーがない場合は警告を出す（または何もしない）
+    console.warn("Firebase Admin Key is missing. Skip initialization.");
+  }
 }
 
 export const db = getFirestore();
