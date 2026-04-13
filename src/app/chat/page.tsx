fa607b69e6de3ebcb45193daa00defe6e26ac9ed
Kotarou628 <Tate62Tora8@gmail.com>
2026-04-13 22:59:55 +0900

    fix: move use client to top

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,6 @@
 // src/app/chat/page.tsx
-export const dynamic = 'force-dynamic';
 'use client'
+export const dynamic = 'force-dynamic';
 
 import React, {
   useEffect, useMemo, useRef, useState, ReactNode,
