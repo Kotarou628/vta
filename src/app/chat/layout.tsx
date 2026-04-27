369d45a96d1aaa5d6093e274e80b13daea31c6f7
Kotarou628 <Tate62Tora8@gmail.com>
2026-04-27 18:07:32 +0900

    URL名前再変更

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "受講生用", 
+  title: "Virtual_TA:受講生用", 
   // ルートのlayout.tsxで設定したテンプレートにより
   // 自動的に「Virtual TA/受講生用」となります
 };
