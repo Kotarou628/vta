8ec463e038aa6fdef0aa162c211a85bda8feb916
Kotarou628 <Tate62Tora8@gmail.com>
2026-04-27 17:52:33 +0900

    ログイン画面名前変更

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "受講生用", 
+  // ルートのlayout.tsxで設定したテンプレートにより
+  // 自動的に「Virtual TA/受講生用」となります
+};
+
+export default function ChatLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
